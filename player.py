3c14fc1a76e1be9142ebe530ef7ac284fc99edb9
Shrushiv Telang <shrushiv.telang@education.nsw.gov.au>
2026-02-15 23:25:36 +0000

    Test commit 2

--- player.py
+++ player.py
@@ -2,7 +2,9 @@ import pygame
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self,pos,groups):
+    def __init__(self, pos, groups, obstacles):
         super().__init__(groups)
-        self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
+        self.image = pygame.Surface((64,64))
+        self.image.fill("red")
         self.rect = self.image.get_rect(topleft = pos)
+        self.obstacles = obstacles
