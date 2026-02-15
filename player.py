dd864c37b1c190a646595826362bbd63d32efed9
Shrushiv Telang <shrushiv.telang@education.nsw.gov.au>
2026-02-15 23:57:06 +0000

    test commit 4

--- player.py
+++ player.py
@@ -4,6 +4,6 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, obstacles):
         super().__init__(groups)
-        self.image = pygame.image.load('/workspaces/SOEN-MP---Game/assets/test player.png').convert_alpha()
+        self.image = pygame.image.load('assets/test player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         self.obstacles = obstacles
--- tile.py
+++ tile.py
@@ -4,5 +4,5 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
         super().__init__(groups)
-        self.image = pygame.image.load('/workspaces/SOEN-MP---Game/assets/ground.png').convert_alpha()
+        self.image = pygame.image.load('assets/ground.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
