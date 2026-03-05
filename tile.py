29ae0ccb262a4894c154eace06d0bc8f4e10ccdd
Shrushiv Telang <shrushiv.telang@education.nsw.gov.au>
2026-03-05 01:25:11 +0000

    test commit

--- tile.py
+++ tile.py
@@ -4,5 +4,5 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
         super().__init__(groups)
-        self.image = pygame.image.load('assets/test/ground.png').convert_alpha()
+        self.image = pygame.image.load('assets/grass.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
