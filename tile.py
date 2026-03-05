3869613519ffb7da85ea1a3e0d1f0790cde05ef3
Shrushiv Telang <shrushiv.telang@education.nsw.gov.au>
2026-03-05 23:11:03 +0000

    Created a hitbox for the player and replaced rect with hitbox for collision detection.

--- tile.py
+++ tile.py
@@ -6,4 +6,5 @@ class Tile(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('assets/grass.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(0,-10)
         self.obstacle_sprites = obstacle_sprites
