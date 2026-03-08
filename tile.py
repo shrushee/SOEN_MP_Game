c8fb70d22160bec6ef32a279c936da724e5d352a
Shrushiv Telang <shrushiv.telang@education.nsw.gov.au>
2026-03-08 23:11:35 +0000

    Replaced the tile sprite for testing purposes

--- tile.py
+++ tile.py
@@ -4,7 +4,7 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,groups,obstacle_sprites):
         super().__init__(groups)
-        self.image = pygame.image.load('assets/grass.png').convert_alpha()
+        self.image = pygame.image.load('assets/seat.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0,-10)
         self.obstacle_sprites = obstacle_sprites
