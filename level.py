e814324785d0fab9d889ea718687e121d8886455
Shrushiv Telang <shrushiv.telang@education.nsw.gov.au>
2026-04-16 06:31:04 +0000

    fixed indentation error

--- level.py
+++ level.py
@@ -26,10 +26,10 @@ class Level:
         for row_index,row in enumerate(layout):
             for col_index, col in enumerate(row): 
                 if col != '-1':
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if style == 'boundary':
-                    Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'invisible')
+                    x = col_index * TILESIZE
+                    y = row_index * TILESIZE
+                    if style == 'boundary':
+                        Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'invisible')
             #    if col == 'x':
              #       Tile((x,y),[self.visible_sprites,self.obstacle_sprites], self.obstacle_sprites)
               #  if col == 'p': 
