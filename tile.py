ad41e156de191e5a3a0aa4cdc7bedc415614f089
Shrushiv Telang <shrushiv.telang@education.nsw.gov.au>
2026-04-16 06:29:15 +0000

    Adding more collision tiles and a floor to the level.

--- tile.py
+++ tile.py
@@ -2,9 +2,10 @@ import pygame
 from settings import *
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self,pos,groups,obstacle_sprites):
+    def __init__(self,pos,groups,obstacle_sprites,sprite_type,surface = pygame.Surface((TILESIZE,TILESIZE))):
         super().__init__(groups)
-        self.image = pygame.image.load('assets/seat.png').convert_alpha()
+        self.sprite_type = sprite_type
+        self.image = surface
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0,-10)
         self.obstacle_sprites = obstacle_sprites
